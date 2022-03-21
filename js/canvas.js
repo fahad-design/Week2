d6d71da539e0b5d94dd9e6d559ef49a4315e5366
fahad-design <buttf5169@gmail.com>
2022-03-21 15:50:34 +0000

    canva

--- js/canvas.js
+++ js/canvas.js
@@ -1,12 +1,11 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
-
 canvas.width = 600;
 canvas.height = 600;
 
+/* Upload Image */
 const readinputfile = new FileReader();
 const readinputimg = new Image();
-
 const imgupload = (e) => {
   readinputfile.onload = () => {
     readinputimg.onload = () => {
@@ -18,56 +17,68 @@ const imgupload = (e) => {
   }
   readinputfile.readAsDataURL(e.target.files[0]);
 }
-
 const loadimg = document.getElementById("uploader");
 loadimg.addEventListener("change", imgupload);
 
-document.getElementById("text").onkeyup = function(){
-  const text = document.getElementById("text").value;
-  drawTextOnCanvas(text, canvas, ctx);
+/* Header Text */
+document.getElementById("header").onkeyup = function(){
+  const header = document.getElementById("header").value;
+  drawTextOnheader(header, canvas, ctx);
 }
 
-function drawTextOnCanvas(text, canvas, ctx){
+function drawTextOnheader(header, canvas, ctx){
+  ctx.font = "20px Arial"
+  const xh = document.getElementById("x-h").value;
+  ctx.fillText(header, xh, 40)
+}
+
+/* Body Text */
+document.getElementById("body").onkeyup = function(){
+  const body = document.getElementById("body").value;
+  drawTextOnCanvas(body, canvas, ctx);
+}
+
+function drawTextOnCanvas(body, canvas, ctx){
   ctx.font = "30px Arial"
-  const x = document.getElementById("text1").value;
-  const y = document.getElementById("text2").value;
-  printAt(ctx, text, x, y, 10, 400)
+  const xb = document.getElementById("x-b").value;
+  ctx.fillText(body, xb, canvas.height/2)
 }
 
-function printAt(ctx, text, x, y, lineHeight, fitWidth) {
-  if (fitWidth <= 0) {
-    ctx.fillText(text, x, y);
-    return;
-  }
-  for (var idx = 1; idx <= text.length; idx++) {
-    var str = text.substr(0, idx);
-    console.log(str, ctx.measureText(str).width, fitWidth);
-    if (ctx.measureText(str).width >= fitWidth) {
-      ctx.fillText(text.substr(0, idx - 1), x, y);
-      printAt(ctx, text.substr(idx - 1), x, y + lineHeight, lineHeight, fitWidth);
-      return;
-    }
-  }
-  ctx.fillText(text, x, y);
+/* Footer Text */
+document.getElementById("footer").onkeyup = function(){
+  const footer = document.getElementById("footer").value;
+  drawTextOnfooter(footer, canvas, ctx);
 }
 
-  $('input').keydown(function(e){
-    if(e.keyCode==8){
-      var newvalue = $(this).val();
-      ctx.clearRect(0,0,canvas.width,canvas.height);
-      ctx.fillText(newvalue, 0, 0);
-    }
-  });
+function drawTextOnfooter(footer, canvas, ctx){
+  ctx.font = "20px Arial"
+  const xf = document.getElementById("x-f").value;
+  ctx.fillText(footer, xf, canvas.height-30)
+}
 
+/* Delete Text */
+$('input').keydown(function (e) {
+  if (e.keyCode == 8) {
+    ctx.clearRect(0, 0, canvas.width, canvas.height);
+    ctx.fillText($(this).val(), 0, 0);
+  }
+});
+
+/* Download Image */
 document.getElementById("download").onclick = function(){
   const link = document.createElement("a");
   link.download = "image.png"
   link.href = document.getElementById("canvas").toDataURL();
   link.click();
 }
+
+/* Clear Every Field on Reload */
 window.addEventListener("load", () => {
-  const text = document.getElementById("text").value = "";
-  const loadimg = document.getElementById("uploader").value = "";
-  const x = document.getElementById("text1").value = "";
-  const y = document.getElementById("text2").value = "";
+  document.getElementById("header").value = "";
+  document.getElementById("body").value = "";
+  document.getElementById("footer").value = "";
+  document.getElementById("uploader").value = "";
+  document.getElementById("x-h").value = "";
+  document.getElementById("x-b").value = "";
+  document.getElementById("x-f").value = "";
 });
